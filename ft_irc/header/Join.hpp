6cd15e3c2195255c6e37c933c29d584b69156425
dkglee <ideade9@gmail.com>
2023-11-21 14:23:50 +0900

    add Makefile

--- ft_irc/header/Join.hpp
+++ ft_irc/header/Join.hpp
@@ -8,7 +8,7 @@ private:
 	std::string channel;
 	std::string key;
 	bool authUser(Channel*, r_list::iterator);
-	void broadcast(Channel*, int);
+	void broadcast(Channel*, r_list::iterator, int);
 public:
 	void setJoin(std::string chn, std::string key);
 	virtual int runOperation(Channel* chs, r_list& ru_list, b_list& bu_list, int fd, int key);
